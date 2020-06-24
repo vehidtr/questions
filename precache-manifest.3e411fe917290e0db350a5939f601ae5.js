1605dcb45bd6cc3511bf14e6c7cba523ab4d80a3
Vehid Trtak <vehit.trtak@gmail.com>
2020-06-24 23:35:45 +0200

    Updates

---
+++ precache-manifest.3e411fe917290e0db350a5939f601ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4794ab5d17bf15ae246d867097ac5175",
+    "url": "/questions/index.html"
+  },
+  {
+    "revision": "db5c343fed8f9ad4a325",
+    "url": "/questions/static/css/main.ab7d6662.chunk.css"
+  },
+  {
+    "revision": "5b8f2394616108c2be18",
+    "url": "/questions/static/js/2.a7ee9c6e.chunk.js"
+  },
+  {
+    "revision": "db5c343fed8f9ad4a325",
+    "url": "/questions/static/js/main.5f4a8c40.chunk.js"
+  },
+  {
+    "revision": "3c419a8983f5434ddbf7",
+    "url": "/questions/static/js/runtime~main.54684bfb.js"
+  }
+]);
